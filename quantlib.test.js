9fed2e68365d145886c88ec63d87c04bec10a1db
Andreas Lindh <andreas.lindh@loopstone.com>
2019-06-03 13:19:32 +0200

    Added npm test to circleci

--- quantlib.test.js
+++ quantlib.test.js
@@ -1,19 +1,22 @@
 var QuantLibModule = require("./quantlib");
 var QuantLib = null;
 
-beforeAll(async () => {
-    var loader = QuantLibModule();
-    loader.ready = () =>
-        new Promise((resolve, reject) => {
-            delete loader.then;
-            loader.onAbort = reject;
-            loader.addOnPostRun(() => {
-                resolve(loader);
+describe("captor/quantlib", () => {
+    beforeAll(async () => {
+        var loader = QuantLibModule();
+        loader.ready = () =>
+            // https://github.com/emscripten-core/emscripten/issues/5820
+            new Promise((resolve, reject) => {
+                delete loader.then;
+                loader.onAbort = reject;
+                loader.addOnPostRun(() => {
+                    resolve(loader);
+                });
             });
-        });
-    QuantLib = await loader.ready();
-});
+        QuantLib = await loader.ready();
+    });
 
-test("Sweden Calendar", async () => {
-    expect(QuantLib.Sweden.name()).toBe("Sweden");
+    test("Sweden Calendar", async () => {
+        expect(QuantLib.Sweden.name()).toBe("Sweden");
+    });
 });
